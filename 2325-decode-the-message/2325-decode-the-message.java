cf607016bb3e4c82d25834929588b323ea6753ab
D Nilesh Choudhary <choudharynilesh60309@gmail.com>
2026-09-06 16:52:36 +0530

    Time: 7 ms (68.78%), Space: 45.1 MB (48.5%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,22 +1,22 @@
 class Solution {
     public String decodeMessage(String key, String message) {
         StringBuilder sb = new StringBuilder();
-        HashMap<Character, Integer> map = new HashMap<>();
-        int j = 0 ; 
+        HashMap<Character, Character> map = new HashMap<>();
+        char strt = 'a';
         for(int i = 0 ; i<key.length() ; i++)
         {
             while(key.charAt(i)==' ')      i++;
             if(i<key.length())
             {
                 if(!map.containsKey(key.charAt(i)))
-                    map.put(key.charAt(i),j++);
-                if(j==26)   break;
+                    map.put(key.charAt(i),strt++);
+                if(strt-1=='z')   break;
             }
         }
         for(int i = 0 ; i<message.length() ; i++ )
         {
             if(message.charAt(i)==' ')  sb.append(' ');
-            else sb.append((char)(map.get(message.charAt(i)).intValue()+97));
+            else sb.append(map.get(message.charAt(i)));
         }
         return sb.toString();
     }
